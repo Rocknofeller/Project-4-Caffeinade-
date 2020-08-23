37c3ae211a32ff0231021c5166af6dbf66acb8d7
Jesse <jesseanderton@hotmail.com>
2020-08-24 09:58:35 +1000

    fONTS AND cODE

--- Assets/Scripts/ShootCoffee.cs
+++ Assets/Scripts/ShootCoffee.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootCoffee : MonoBehaviour
 {
@@ -13,6 +14,16 @@ public class ShootCoffee : MonoBehaviour
     public bool shootAble = true;
     public float waitBeforeNextShot = 0.25f;
 
+    public float bulletCount;
+    public float maxBullets;
+    public float reloadTime;
+    public Text AmmoCount;
+
+
+    private void Start()
+    {
+        bulletCount = maxBullets;
+    }
     private void Update()
     {
         if (Input.GetKey(KeyCode.Mouse0))
@@ -24,18 +35,41 @@ public class ShootCoffee : MonoBehaviour
                 StartCoroutine(ShootingYield());
             }
         }
+
+        AmmoCount.text = bulletCount.ToString("0");
     }
 
     IEnumerator ShootingYield()
     {
-        yield return new WaitForSeconds(waitBeforeNextShot);
-        shootAble = true;
+        if (bulletCount != 0)
+        {
+            yield return new WaitForSeconds(waitBeforeNextShot);
+            shootAble = true;
+        }
+
     }
     void Shoot()
     {
         var bullet = Instantiate(coffee, playerHolder.position, playerHolder.rotation);
         bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
 
+        bulletCount -= 1;
+        Debug.Log(bulletCount);
+        if (bulletCount == 0)
+        {
+            Debug.Log("Reloading");
+            shootAble = false;
+            StartCoroutine(ReloadCoroutine());
+        }
+
         Destroy(bullet, despawnTime);
+    }
+
+    IEnumerator ReloadCoroutine()
+    {
+        yield return new WaitForSeconds(reloadTime);
+        bulletCount = maxBullets;
+        shootAble = true;
+        Debug.Log("Reloaded");
     }
 }
