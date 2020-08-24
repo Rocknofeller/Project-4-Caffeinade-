b70c13bac599d4dbbe089de402e004a88ddc53b0
Noel A <1015895@student.sae.edu.au>
2020-08-25 05:38:01 +1000

    New Characters
    Putting new characters seating them in place

---
+++ Assets/Scripts/CameraAnim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnim : MonoBehaviour
+{
+    public float xRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xRot = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown (0))
+        {
+            xRot = .5f;
+        }
+
+        this.gameObject.transform.Rotate(xRot, 0f, 0f);
+
+        if (this.gameObject.transform.rotation.x >= 0f)
+        {
+            xRot = 0;
+        }
+    }
+
+}
